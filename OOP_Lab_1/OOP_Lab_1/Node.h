67bdf40319a039cfa2245e03a2d30369de120ede
DoctorMatt <DoctorMatt47@gmail.com>
2020-09-06 17:38:37 +0300

    Added class Node

---
+++ OOP_Lab_1/OOP_Lab_1/Node.h
@@ -0,0 +1,70 @@
+#pragma once
+
+template <class T>
+class Node
+{
+private:
+	T _data;
+	Node<T>* _pNext;
+	Node<T>* _pPrev;
+public:
+	
+	explicit Node(T data, Node* pPrev = nullptr, Node* pNext = nullptr);
+	
+	T GetData();
+	
+	void SetData(T data);
+
+	Node* GetNext();
+
+	void SetNext(Node<T>* pNext);
+
+	Node* GetPrev();
+
+	void SetPrev(Node<T>* pPrev);
+
+};
+
+template<class T>
+inline Node<T>::Node(T data, Node* pPrev, Node* pNext)
+{
+	_data = data;
+	_pPrev = pPrev;
+	_pNext = pNext;
+}
+
+template<class T>
+inline T Node<T>::GetData()
+{
+	return _data;
+}
+
+template<class T>
+inline void Node<T>::SetData(T data)
+{
+	_data = data;
+}
+
+template<class T>
+inline Node<T>* Node<T>::GetNext()
+{
+	return _pNext;
+}
+
+template<class T>
+inline void Node<T>::SetNext(Node<T>* pNext)
+{
+	_pNext = pNext;
+}
+
+template<class T>
+inline Node<T>* Node<T>::GetPrev()
+{
+	return _pPrev;
+}
+
+template<class T>
+inline void Node<T>::SetPrev(Node<T>* pPrev)
+{
+	_pPrev = pPrev;
+}
